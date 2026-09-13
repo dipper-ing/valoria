84cee38f129c33764c7618eb11a6472e50466d6d
dipper-ing <juanjo222e@gmail.com>
2026-09-13 14:03:22 -0500

    feat: agrega entidad Usuario y enum Role (WIP, falta ddl-auto) (#3)

---
+++ backend/src/main/java/com/valoria/backend/model/Usuario.java
@@ -0,0 +1,71 @@
+package com.valoria.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "usuario")
+public class Usuario {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenaHash() {
+        return contrasenaHash;
+    }
+
+    public void setContrasenaHash(String contrasenaHash) {
+        this.contrasenaHash = contrasenaHash;
+    }
+
+    public String getProveedorAuth() {
+        return proveedorAuth;
+    }
+
+    public void setProveedorAuth(String proveedorAuth) {
+        this.proveedorAuth = proveedorAuth;
+    }
+
+    public String getGoogleRefreshToken() {
+        return googleRefreshToken;
+    }
+
+    public void setGoogleRefreshToken(String googleRefreshToken) {
+        this.googleRefreshToken = googleRefreshToken;
+    }
+
+    public Role getRol() {
+        return rol;
+    }
+
+    public void setRol(Role rol) {
+        this.rol = rol;
+    }
+
+    @Id
+    @GeneratedValue(strategy = generationType.IDENTITY)
+    private  Long id;
+
+    @Column(unique = true = true, nullable = false)
+    private String correo;
+
+    private String contrasenaHash;
+
+    private String proveedorAuth;
+
+    private String googleRefreshToken;
+
+    @Enumerated(EnumType.STRING)
+    private Role rol;
+}
